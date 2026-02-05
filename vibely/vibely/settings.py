011522b5b44b3b229db33f94b30a266ca5e970d0
SagarDevop <sagar.singh44818gmail.com>
2026-02-06 03:42:49 +0530

    Update WebSocket URL regex to allow room names with word characters and configure Redis connection with SSL

--- vibely/vibely/settings.py
+++ vibely/vibely/settings.py
@@ -100,7 +100,12 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get("REDIS_URL")],
+            "hosts": [
+                {
+                    "address": os.environ["REDIS_URL"],
+                    "ssl": True,
+                }
+            ],
         },
     },
 }
